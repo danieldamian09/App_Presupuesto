245fa3ba91b97c85fa700fefd445b81928790683
humberto <humbertorivero9@gmail.com>
2021-05-21 11:50:54 -0300

    creando nuevo gasto, listando en el state y reseteando formulario

--- presupuesto/src/App.js
+++ presupuesto/src/App.js
@@ -9,6 +9,15 @@ function App() {
   const [restante, guardarRestante] = useState(0)
   // estado para mostrar el componente Pregunta condicionado (depende de si ya se definio un presupuesto)
   const [mostrarpregunta, actualizarPregunta] = useState(true)
+  const [gastos, guardarGasto] = useState([]);
+
+  // funcion cuando se agrega un nuevo gasto
+  const agregarNuevoGasto = gasto => {
+    guardarGasto([
+      ...gastos,
+      gasto
+    ])
+  }
 
   return (
     <div className="container">
@@ -27,7 +36,9 @@ function App() {
             : (
               <div className="row">
                 <div className="one-half column">
-                  <Formulario />
+                  <Formulario
+                    agregarNuevoGasto={agregarNuevoGasto}
+                  />
                 </div>
                 <div className="one-half column">
                   2
